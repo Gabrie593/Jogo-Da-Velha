ffdb5105d240a4f06d48eec153ef8ad010f08e05
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2019-04-19 07:55:12 -0300

    versão final 1.0 com matriz

--- Jogo da Velha Comentado 3.0.py
+++ Jogo da Velha Comentado 3.0.py
@@ -1,122 +1,77 @@
-# Bibliotecas que serão usadas
+# Bibliotecas #
 import random
 
-# Variaveis que representam cada posição
-p1 = "1"
-p2 = "2"
-p3 = "3"
-p4 = "4"
-p5 = "5"
-p6 = "6"
-p7 = "7"
-p8 = "8"
-p9 = "9"
+# Tabuleiro #
+board = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+collumn1 = (board[0][0]), (board[1][0]), (board[2][0])
+collumn2 = (board[0][1]), (board[1][1]), (board[2][1])
+collumn3 = (board[0][2]), (board[1][2]), (board[2][2])
+diagonal1 = (board[0][0]), (board[1][1]), (board[2][2])
+diagonal2 = (board[0][2]), (board[1][1]), (board[2][0])
 
-# Construção do Tabuleiro
-collumdiv = "|"
-boarddiv = "\n---------"
-linha1 = p1 + collumdiv + p2 + collumdiv + p3 + boarddiv
-linha2 = p4 + collumdiv + p5 + collumdiv + p6 + boarddiv
-linha3 = p7 + collumdiv + p8 + collumdiv + p9 
-diagonal1 = (p3, p5, p7)
-diagonal2 = (p1, p5, p9)
-coluna1 = (p1, p4, p7)
-coluna2 = (p2, p5, p8)
-coluna3 = (p4, p6, p9)
-bigboard = ((linha1) + '\n' + (linha2) + '\n' + (linha3))
+print (board[0])
+print (board[1])
+print (board[2])
 
-# Coletar os nomes dos jogadores, e decidir quem deverá começar
-nome1 = input("Digite o nome do jogador que representa [x]: ")
-nome2 = input("Digite o nome do jogador que representa [O]: ")
-iniciante = " "
-contrainiciante = " "
-if (random.random() * 100) < 50:
-    iniciante = nome1
-    contrainiciante = nome2
+# A jogada do usuário #
+# Decidir quem começa #
+iniciante = (" ")
+contrainiciante = (" ")
+if random.random() * 100 < 50:
+    iniciante = ("X")
+    contrainiciante = ("O")
 else:
-    iniciante = nome2
-    contrainiciante = nome1
-
-# Variáveis relativas
-quantidade = 0
-validade = 0
-caracter = (" ")
-again = (" ")
-
-# Mostrar o Tabuleiro
-print (bigboard)
-
-# O jogo de fato
-while (quantidade < 9):
-    if (quantidade % 2) != 0:
-        caracter = "x"
+    iniciante = ("O")
+    contrainiciante = ("X")
+jogada = 1
+# indicar se é a vez do X ou da O #
+while jogada < 9:
+    # checando se tem vitória #
+    vitoriax = ['X', 'X', 'X']
+    if (board[0]) == (vitoriax) or (board[1]) == (vitoriax) or (board[2]) == (vitoriax):
+        print ("X won!")
+        break
+    elif (collumn1) == (vitoriax) or (collumn2) == (vitoriax) or (collumn3) == (vitoriax):
+        print ("X won!")
+        break
+    elif diagonal1 == (vitoriax) or diagonal2 == (vitoriax):
+        print ("X won!")
+        break
+    if (jogada % 2) != 0:
+        caracter = iniciante
+        print ("It's", iniciante, "turn!")
     else:
-        caracter = "O"
+        caracter = contrainiciante
+        print ("It's", contrainiciante, "turn!")
 
-    if (quantidade % 2) == 0:
-        position1 = input("Agora é a vez de " + iniciante.lower().capitalize() + ", Digite qual posição você quer jogar: ")
+    movel = int(input("Type the line that you wanna play: "))
+    movec = int(input("Type the collumn that you wanna play: "))
+    # checando a validade da jogada #
+    if type(board[movel][movec]) == str:
+        print ("Invalid move, try again!")
     else:
-        position1 = input("Agora é a vez de " + contrainiciante.lower().capitalize() + ", Digite qual posição você quer jogar: ")
+        board[movel][movec] = (caracter)
+        jogada = jogada + 1
 
-    validade = 0
+        print (board[0])
+        print (board[1])
+        print (board[2])
 
-    if position1 == ("1") and p1 == "1":
-        p1 = caracter 
-    elif position1 == ("2") and p2 == "2":
-        p2 = caracter
-    elif position1 == ("3") and p3 == "3":
-        p3 = caracter
-    elif position1 == ("4") and p4 == "4":
-        p4 = caracter
-    elif position1 == ("5") and p5 == "5":
-        p5 = caracter
-    elif position1 == ("6") and p6 == "6":
-        p6 = caracter
-    elif position1 == ("7") and p7 == "7":
-        p7 = caracter
-    elif position1 == ("8") and p8 == "8":
-        p8 = caracter
-    elif position1 == ("9") and p9 == "9":
-        p9 = caracter
 
-    # Checando validade da jogada
-    if validade == 1 and position1 == ("1") or position1 == ("2") or position1 == ("3") or position1 == ("4") or position1 == ("5") or position1 == ("6") or position1 == ("7") or position1 == ("8") or position1 == ("9"):
-        quantidade = quantidade + 1
 
-        # Atualizando variáveis relativas
-        linha1 = p1 + collumdiv + p2 + collumdiv + p3 + boarddiv
-        linha2 = p4 + collumdiv + p5 + collumdiv + p6 + boarddiv
-        linha3 = p7 + collumdiv + p8 + collumdiv + p9
-        diagonal1 = (p3, p5, p7)
-        diagonal2 = (p1, p5, p9)
-        bigboard = ((linha1) + '\n' + (linha2) + '\n' + (linha3))
-        print (bigboard)
 
-        # Checando se aconteceu uma vitória ou uma velha
-        if linha1 == 'O|O|O\n---------' or linha2 == 'O|O|O\n---------' or linha3 == 'O|O|O\n---------':
-            print ("Vitória de " + nome2)
-            break
-        if diagonal1 == ("O", "O", "O") or diagonal2 == ("O", "O", "O"):
-            print ("Vitória de " + nome2)
-            break
-        if coluna1 == ("O", "O", "O") or coluna2 == ("O", "O", "O") or coluna3 == ("O", "O", "O"):
-            print ("Vitória de " + nome2)
-            break
-        if linha1 == 'x|x|x\n---------' or linha2 == 'x|x|x\n---------' or linha3 == 'x|x|x\n---------':
-            print ("Vitória de " + nome1)
-            break
-        if diagonal1 == ("x", "x", "x") or diagonal2 == ("x", "x", "x"):
-            print ("Vitória de " + nome1)
-            break
-        if coluna1 == ("x", "x", "x") or coluna2 == ("x", "x", "x") or coluna3 == ("x", "x", "x"):
-            print ("Vitória de " + nome1)
-            break
-        elif quantidade == 9:
-            print ("Deu velha!")
-            break
+
+
+
+
+    
+
+    
+
      
-    else:
-        print ("Jogada inválida, tente novamente!")
+   
 
 
 
